d4f04d5a695c06e64018372fe405dacca1ab7f1f
aleluiz73 <aleluiz73@gmail.com>
2019-11-06 21:04:40 -0300

    aula-08

---
+++ aula-08/js/script.js
@@ -0,0 +1,10 @@
+let anime = ["death note","boku ni hero","naruto","shingeki no kigin"];
+// let i = 0;
+
+// while(i < anime.length){
+//     console.log(anime[i]);
+//     i++;
+// }
+for(let ta = 0; ta < anime.length; ta++){
+    console.log(anime[ta]); 
+}
