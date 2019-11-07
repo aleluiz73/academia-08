223886713dfca656fbce8f0ca1c09af5f8a19e27
aleluiz73 <aleluiz73@gmail.com>
2019-11-06 21:49:29 -0300

    aula-08

--- aula-08/js/script.js
+++ aula-08/js/script.js
@@ -1,10 +1,18 @@
-let anime = ["death note","boku ni hero","naruto","shingeki no kigin"];
-// let i = 0;
+// let anime = ["death note","boku ni hero","naruto","shingeki no kigin"];
+// // let i = 0;
 
-// while(i < anime.length){
-//     console.log(anime[i]);
-//     i++;
+// // while(i < anime.length){
+// //     console.log(anime[i]);
+// //     i++;
+// // }
+// for(let ta = 0; ta < anime.length; ta++){
+//     console.log(anime[ta]); 
 // }
-for(let ta = 0; ta < anime.length; ta++){
-    console.log(anime[ta]); 
+
+let material = prompt("Qual é o seu material?");
+let tamanhoEscada = Number(prompt("Qual o tamanho da sua escada em números?"));
+let escada = "";
+
+for (let index = 0; index < tamanhoEscada; index++) {
+    console.log(escada = escada + material);
 }
