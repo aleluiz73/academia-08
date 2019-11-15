735b47bfc088900f46812d81e18f9529c9879cdd
unknown <aleluiz73@gmail.com>
2019-11-14 21:26:34 -0300

    um bucado de coisa e os bang do jngou

---
+++ aula-09/js/script.js
@@ -0,0 +1,11 @@
+let botao = document.querySelector("button");
+
+
+
+function my(){
+    document.querySelector('h1').innerHTML = "teste1";
+}
+
+botao.onclick = my;
+
+// botao classlist.toggle(my)
---
+++ python/milera.py
@@ -0,0 +1,21 @@
+# nome = input("digiteno seu nome").lower
+
+# if nome == 'pedro':
+#     print("salve cusao")
+# elif nome == 'murilo':
+#     print("ah nao")
+# else:
+#     print("vai cagar")
+# nomero = 0
+# while nomero < 5:
+#     print(nomero)
+#     nomero = nomero + 1
+
+lista = ['cubo', 'slw', 'mika']
+# for item in lista:
+#     print(item)
+
+for i in range(0, len(lista)):
+    print(i)
+    print(lista[i])
+    
